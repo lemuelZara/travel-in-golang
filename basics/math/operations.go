3f1d6ab793527cf39a37565618116b2824e0dc2f
Lemuel <lemuel.czara@gmail.com>
2022-05-30 20:50:41 -0300

    refactor: adjust math operations

--- basics/math/operations.go
+++ basics/math/operations.go
@@ -2,20 +2,36 @@ package math
 
 import "math"
 
-func Sum(num1 float64, num2 float64) float64 {
+func Round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func ToFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+
+	return float64(Round(num*output)) / output
+}
+
+func Sum(num1, num2 float64) float64 {
 	return num1 + num2
 }
 
-func Sub(num1 float64, num2 float64) float64 {
-	result := math.Trunc(num1 - num2)
+func SumAll(nums []float64) (result float64) {
+	for _, v := range nums {
+		result += v
+	}
 
 	return result
 }
 
-func Mult(num1 float64, num2 float64) float64 {
-	return num1 * num2
+func Sub(num1, num2 float64) float64 {
+	return ToFixed(num1-num2, 2)
 }
 
-func Div(num1 float64, num2 float64) float64 {
-	return num1 / num2
+func Mult(num1, num2 float64) float64 {
+	return ToFixed(num1*num2, 2)
+}
+
+func Div(num1, num2 float64) float64 {
+	return ToFixed(num1/num2, 2)
 }
--- basics/math/operations_test.go
+++ basics/math/operations_test.go
@@ -7,9 +7,30 @@ import (
 )
 
 func Test_Operations(t *testing.T) {
+	t.Run("should round float number", func(t *testing.T) {
+		expected := 12
+		result := Round(12.43)
+
+		assert.Equal(t, result, expected)
+	})
+
+	t.Run("should fixed float number", func(t *testing.T) {
+		expected := 49.3919
+		result := ToFixed(49.3918687, 4)
+
+		assert.Equal(t, result, expected)
+	})
+
 	t.Run("should return correct sum", func(t *testing.T) {
 		expected := 7.33
 		result := Sum(2.34, 4.99)
+
+		assert.Equal(t, result, expected)
+	})
+
+	t.Run("should return correct sum all numbers of array", func(t *testing.T) {
+		expected := 22.3702
+		result := SumAll([]float64{1.254, 1.933, 7.4012, 3.6, 8.182})
 
 		assert.Equal(t, result, expected)
 	})
@@ -22,14 +43,14 @@ func Test_Operations(t *testing.T) {
 	})
 
 	t.Run("should return correct multiplication", func(t *testing.T) {
-		expected := 7.33
+		expected := 11.68
 		result := Mult(2.34, 4.99)
 
 		assert.Equal(t, result, expected)
 	})
 
 	t.Run("should return correct division", func(t *testing.T) {
-		expected := 7.33
+		expected := 0.47
 		result := Div(2.34, 4.99)
 
 		assert.Equal(t, result, expected)
