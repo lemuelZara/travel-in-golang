a13e9176b461b459be8ec0ccee2a06905ab2762a
Lemuel <lemuel.czara@gmail.com>
2022-06-08 21:51:27 -0300

    refactor: encode strings

--- basics/encode.go
+++ basics/encode.go
@@ -10,3 +10,36 @@ func ConvertString(str string) (string, []byte) {
 
 	return hex, bytes
 }
+
+func SpamMask(str string) []byte {
+	size := len(str)
+	buf := []byte(str)
+	in := false
+
+	const (
+		link  = "http://"
+		nlink = len(link)
+		mask  = '*'
+	)
+
+	for i := 0; i < size; i++ {
+		if len(str[i:]) >= nlink && str[i:i+nlink] == link {
+			in = true
+
+			i += nlink
+		}
+
+		c := str[i]
+
+		switch c {
+		case ' ', '\t', '\n':
+			in = false
+		}
+
+		if in {
+			buf[i] = mask
+		}
+	}
+
+	return buf
+}
